f1b2fded70c98efc898404624a79dd8d443165c6
Harshvardhan Sharma <102682161+hvs1234@users.noreply.github.com>
2023-05-06 20:03:21 +0530

    Create BOM,DOM And Window Object.js

---
+++ BOM,DOM And Window Object.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <title>Javascript Logic</title>
+    <script src="BOM,DOM And Window Object.js"></script>
+</head>
+
+<script>
+    console.log(window);
+    window.console.log(window);
+    
+    f = ()=> {
+        location.href = "https://react.dev"
+    }
+</script>
+
+<body>
+    <div class="container-1">
+        <h1>Welcome to my website</h1>
+        <button onclick="f()">Click</button>
+    </div>
+</body>
+</html>
