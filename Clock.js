1cf0f791bc093bf0cd212fd555fa1d1ee039b6ae
Harshvardhan Sharma <102682161+hvs1234@users.noreply.github.com>
2023-05-08 09:27:50 +0530

    Create Clock.js

---
+++ Clock.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Javascript Logic</title>
+    <script src="Clock.js"></script>
+</head>
+
+<script>
+    const day = new Date();
+    let h = day.getHours();
+    let m = day.getMinutes();
+    let s = day.getSeconds();
+    let d = day.getDate();
+
+console.log(h,m,s,d);
+</script>
+
+<body>
+    <h1>Hello</h1>
+</body>
+</html>
