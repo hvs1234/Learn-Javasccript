70c3b6a55ab429d587bc0dd901a6c5c24e8f6f19
Harshvardhan Sharma <102682161+hvs1234@users.noreply.github.com>
2023-05-08 09:20:40 +0530

    Create InnerHTML.js

---
+++ InnerHTML.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Javascript Logic</title>
+</head>
+<script>
+    f = ()=> {
+        let s1 = document.getElementById('set');
+        s1.innerHTML = "<h1>Hello</h1>";
+    }
+</script>
+
+<body>
+    <button id="set" onclick="f()">Click</button>
+</body>
+</html>
