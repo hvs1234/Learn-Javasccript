45d34ec65b6b0091f447c6d7a6e849413e82a47c
Harshvardhan Sharma <102682161+hvs1234@users.noreply.github.com>
2023-07-09 10:09:58 +0530

    Update Event Functions.js

--- Event Functions.js
+++ Event Functions.js
@@ -1,22 +1,19 @@
-function togglehide() {
-    let c1 = document.getElementById('btn');
-    let c2 = document.getElementById('para');
-    let c3 = document.getElementById('change');
-    c3.style.color = 'darkblue';
-    c1.style.color="red";
-    if(c2.style.display!='none') {
-        c2.style.display='none';
-        c1.style.color='indigo';
-    }  
-    else {
-        c2.style.display='block';
-    }
+// 4 ways to write a events in javascript 1- By alert(), 2- By calling function, 3- Using inline events, 4- using event listeners.
+
+// By calling function.
+f1 = ()=>
+{
+    alert("You have clicked on this link by using function");
 }
 
-function fontset() {
-    let f1 = document.getElementById('font');
-    let f2 = document.getElementById('para');
-    f1.style.color='darkgreen';
-    f2.style.fontSize='1em'
-    if(f2.style.fontSize='1em') f2.style.fontSize="2em";
+// By inline events.
+const link = document.getElementById('link');
+link.onclick = function() {
+    alert("you clicked on inline event");
 }
+
+// By event listener.
+const link2 = document.querySelector('#link2');
+link2.addEventListener('click',()=>{
+    alert("You clicked on event listener")
+})
